4f497f739383df56a2e6f1397ea281c498a3f654
aasimsyed97 <sayedaasim555@gamail.com>
2023-04-01 01:09:56 +0530

    spring project added

---
+++ clickCart/src/main/java/com/clickCart/ClickCartApplication.java
@@ -0,0 +1,13 @@
+package com.clickCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickCartApplication.class, args);
+	}
+
+}
