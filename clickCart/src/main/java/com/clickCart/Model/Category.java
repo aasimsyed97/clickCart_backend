c59662f0f20b8add4bf5d12e43ce8f07532a95ff
aasimsyed97 <sayedaasim555@gmail.com>
2023-04-04 12:44:54 +0530

    annotation added

--- clickCart/src/main/java/com/clickCart/Model/Category.java
+++ clickCart/src/main/java/com/clickCart/Model/Category.java
@@ -1,11 +1,14 @@
 package com.clickCart.Model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Category { 
 	 @Id
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
 	 
     private Integer categoryId;
     
