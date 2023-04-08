74e4fbca923425d342c8b57415d705771aa8ffca
aasimsyed97 <sayedaasim555@gmail.com>
2023-04-08 23:47:15 +0530

    cart is updated

--- clickCart/src/main/java/com/clickCart/Model/Cart.java
+++ clickCart/src/main/java/com/clickCart/Model/Cart.java
@@ -9,5 +9,36 @@ public class Cart {
       @jakarta.persistence.Id
       @GeneratedValue(strategy = GenerationType.AUTO)
 	 private Integer Id;
+	 private  Integer total;
+	 private String name;
+	public Cart() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Cart(Integer id, Integer total, String name) {
+		super();
+		Id = id;
+		this.total = total;
+		this.name = name;
+	}
+	public Integer getId() {
+		return Id;
+	}
+	public void setId(Integer id) {
+		Id = id;
+	}
+	public Integer getTotal() {
+		return total;
+	}
+	public void setTotal(Integer total) {
+		this.total = total;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	} 
+	 
 	 
 }
