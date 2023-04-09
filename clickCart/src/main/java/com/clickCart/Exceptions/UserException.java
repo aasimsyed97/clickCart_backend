5c3e292f4c7fb33a41342b5eeb3b8f1576ab9eef
aasimsyed97 <sayedaasim555@gmail.com>
2023-04-09 23:57:45 +0530

    user exception

--- clickCart/src/main/java/com/clickCart/Exceptions/UserException.java
+++ clickCart/src/main/java/com/clickCart/Exceptions/UserException.java
@@ -14,4 +14,6 @@ public class UserException extends Exception {
 	
 	
 
+	
+	
 }
--- clickCart/src/main/java/com/clickCart/Service/UserService.java
+++ clickCart/src/main/java/com/clickCart/Service/UserService.java
@@ -1,5 +1,9 @@
 package com.clickCart.Service;
 
+import com.clickCart.Model.User;
+
 public interface UserService {
+	
+	public User createUser(User user);
 
 }
