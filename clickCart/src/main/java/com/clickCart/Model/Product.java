61b80ca015c3f4ca33c342c22aa12245999d768f
aasimsyed97 <sayedaasim555@gmail.com>
2023-04-12 23:27:58 +0530

    update

--- clickCart/src/main/java/com/clickCart/Model/Product.java
+++ clickCart/src/main/java/com/clickCart/Model/Product.java
@@ -38,42 +38,6 @@ public class Product {
 	}
 
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
-
-
-	public String getDescription() {
-		return description;
-	}
-
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-
-	public String getUrl() {
-		return url;
-	}
-
-
-	public void setUrl(String url) {
-		this.url = url;
-	}
-
-
-	public Integer getPrice() {
-		return price;
-	}
-
-
-	public void setPrice(Integer price) {
-		this.price = price;
-	} 
-	
-	
    
 	 
 }
